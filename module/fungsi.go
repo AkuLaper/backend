13543070513e18da793a7a7a59974544c474ca7a
GilangAndhika <gryzlegrizz@gmail.com>
2024-10-14 15:58:44 +0700

    Config: Connect to database

---
+++ module/fungsi.go
@@ -0,0 +1,30 @@
+package module
+
+import (
+	"context"
+	"error"
+	"fmt"
+
+	"github.com/AkuLaper/backend/model"
+
+	// "go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func MongoConnect(dbname string) (db *mongo.Database) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(MongoString))
+	if err != nil {
+		fmt.Println("MongoConnect Error: ", err)
+	}
+	return client.Database(dbname)
+}
+
+func InsertOneDoc(db string, collection string, doc interface{}) (insertedID interface{}) {
+	insertResult, err := MongoConnect(db).Collection(collection).InsertOne(context.TODO(), doc)
+	if err != nil {
+		fmt.Println("InsertOneDoc Error: ", err)
+	}
+	return insertResult.InsertedID
+}
